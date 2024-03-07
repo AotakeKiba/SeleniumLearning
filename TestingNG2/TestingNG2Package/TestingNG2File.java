ca84a534c3d87442f2586c71cc195869dd89d9ff
AotakeKiba <37775077+AotakeKiba@users.noreply.github.com>
2024-03-07 10:01:39 +0700

    Learning TestNG Selenium 2

---
+++ TestingNG2/TestingNG2Package/TestingNG2File.java
@@ -0,0 +1,10 @@
+package TestingNG2Package;
+
+import org.testng.annotations.Test;
+
+public class TestingNG2File {
+  @Test
+  public void f() {
+	  System.out.println("launching firefox browser"); 
+  }
+}
---
+++ TestingNG2/src/Testing1/Testing1File.java
@@ -0,0 +1,9 @@
+package Testing1;
+
+import org.testng.annotations.Test;
+
+public class Testing1File {
+  @Test
+  public void f() {
+  }
+}
